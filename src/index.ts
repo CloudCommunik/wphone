19cb298643b0f1b04df76c8c67c78d5abdc21436
Pedro Sanders <psanders@fonoster.com>
2021-09-18 22:05:28 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import { CallRequest, MessageRequest, WPhoneConfig, InviterConfig } from "./types";
 import WPhone from "./wphone";
 
